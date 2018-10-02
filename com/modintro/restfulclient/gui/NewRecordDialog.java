79e29278aa90d6d2739dcdabd216aa8b66b5500f
unknown <cspen@modintro.com>
2018-10-01 23:52:49 -0500

    Add layout to NewRecordDialog

--- com/modintro/restfulclient/gui/NewRecordDialog.java
+++ com/modintro/restfulclient/gui/NewRecordDialog.java
@@ -2,12 +2,15 @@ package com.modintro.restfulclient.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -18,11 +21,36 @@ public class NewRecordDialog extends JDialog implements ActionListener {
 		this.window = window;
 		setSize(100, 100);
 		
-		JPanel form = new JPanel();
+		JPanel form = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		JLabel fnameLabel = new JLabel("First Name:");
+		form.add(fnameLabel, c);
+		
+		c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 0;
 		JTextField fname = new JTextField();
+		form.add(fname, c);
+		
+		c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 1;
+		JLabel lnameLabel = new JLabel("Last Name:");
+		form.add(lnameLabel, c);
+		
+		c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 1;
 		JTextField lname = new JTextField();
-		form.add(fname);
-		form.add(lname);
+		form.add(lname, c);
+		
+		
 		getContentPane().add(form, BorderLayout.CENTER);
 		
 		JPanel buttonPane = new JPanel();
