60039ce1b24ecb989017b832758a1c8bb49fc047
unknown <cspen@modintro.com>
2018-10-10 23:45:38 -0500

    Add form validation

--- com/modintro/restfulclient/gui/NewRecordDialog.java
+++ com/modintro/restfulclient/gui/NewRecordDialog.java
@@ -8,6 +8,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import javax.swing.JButton;
@@ -16,10 +18,22 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
-public class NewRecordDialog extends JDialog implements ActionListener {
+import com.modintro.restfulclient.model.Constants;
+import com.modintro.restfulclient.model.HTTPRequest;
+
+import test.jaxb.Departments;
+import test.jaxb.Employees;
+import test.jaxb.Employees.Employee;
+
+public class NewRecordDialog extends JDialog
+		implements ActionListener, Constants {
 	
 	public NewRecordDialog(JFrame window) {
 		super(window, "New Record", true);		
@@ -131,7 +145,7 @@ public class NewRecordDialog extends JDialog implements ActionListener {
 		c.insets = new Insets(5, 5, 5, 5);
 		form.add(salLabel, c);
 		
-		JTextField salary = new JTextField();
+		salary = new JTextField();
 		c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 1;
@@ -155,8 +169,27 @@ public class NewRecordDialog extends JDialog implements ActionListener {
 		return button;
 	}
 	
-	private static JComboBox createDeptCombo() {
-		return new JComboBox();
+	private static JComboBox<String> createDeptCombo() {		
+		HTTPRequest httpReq = new HTTPRequest(DEPT_URL);
+		JComboBox<String> deptCombo = new JComboBox<String>();
+		String depts = null;
+		try {
+			depts = httpReq.getData();
+			JAXBContext jbc = JAXBContext.newInstance("test.jaxb");
+			Unmarshaller u = jbc.createUnmarshaller();
+			
+			Departments d = (Departments)u.unmarshal(new StreamSource(new StringReader(depts)));
+		    ArrayList<String> list = (ArrayList<String>) d.getDepartment();
+		    int length = list.size();
+		    for(int i = 0; i < length; i++) {
+		    	deptCombo.addItem(list.get(i));
+		    }
+		   			
+		} catch(Exception e) {
+			System.out.println(e.getMessage());			
+		}
+		
+		return deptCombo;
 	}
 	
 	private static JComboBox<Integer> createNumericCombo(int start, int end, int set) {
@@ -174,22 +207,61 @@ public class NewRecordDialog extends JDialog implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
 		if(source == ok) {
-			// TO-DO: Code to add new record
-			System.out.println("Last Name: " + lname.getText());
-			System.out.println("First Name: " + fname.getText());
-			System.out.println("Department: " + deptCombo.getSelectedItem());
-			System.out.println("Full Time: " + ftCheck.isSelected());
-			System.out.println("Hired Date: " + hireYear.getSelectedItem() + "-" +
-					hireMonth.getSelectedItem() + "-" + hireDay.getSelectedIndex());
-			System.out.println("Salary: " + salary.getText());
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			// setVisible(false);
+			String lastName =  lname.getText();
+			String firstName = fname.getText();
+			String department = (String)deptCombo.getSelectedItem();
+			Boolean fullTime = ftCheck.isSelected();
+			Integer year = (Integer)hireYear.getSelectedItem();
+			Integer  month = (Integer)hireMonth.getSelectedItem();
+			Integer day = (Integer)hireDay.getSelectedItem();
+			String sal = salary.getText();	
+			
+			
+			if(validateNewEmployee(lastName, firstName, year, month, day, sal)) {	
+				HTTPRequest httpReq = new HTTPRequest(DEPT_URL);
+				// setVisible(false);
+			}
 		} else if(source == cancel) {
-			// TO-DO: Clear all inputs
+			reset();
 			setVisible(false);
 		}
+	}
+	
+	private Boolean validateNewEmployee(String ln, String fn,
+			Integer year, Integer month, Integer day, String pay) {
+		String regEx = "^[A-Za-z ]+$";
+		String errMsg = "";
+		if(!ln.matches(regEx)) {
+			errMsg += "Last name may contain only letters and spaces";
+		}
+		if(!fn.matches(regEx)) {
+			errMsg += "\nFirst name my contain only letters and spaces";
+		}
+		if(validateDate(year, month, day)) {
+			errMsg += "\nThe date is not valid";
+		}
+		// if()
+			
+		if(!errMsg.equals("")) {
+			JOptionPane.showMessageDialog(this, errMsg);
+			return false;
+		}		
+		return true;
+	}
+	
+	private Boolean validateDate(Integer year, Integer month, Integer day) {
+		return true;
+	}
+	
+	private void reset() {
+		fname.setText(null);
+		lname.setText(null);
+		salary.setText(null);
+		deptCombo.setSelectedIndex(0);
+		ftCheck.setSelected(false);
+		hireYear.setSelectedItem(Calendar.getInstance().get(Calendar.YEAR));
+		hireMonth.setSelectedItem(Calendar.getInstance().get(Calendar.MONTH));
+		hireDay.setSelectedItem(Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
 	}
 	
 	private JFrame window;
@@ -198,10 +270,12 @@ public class NewRecordDialog extends JDialog implements ActionListener {
 	
 	private JTextField fname;
 	private JTextField lname;
-	private JComboBox deptCombo;
+	private JComboBox<String> deptCombo;
 	private JCheckBox ftCheck;
 	private JComboBox<Integer> hireYear;
 	private JComboBox<Integer> hireMonth;
 	private JComboBox<Integer> hireDay;
 	private JTextField salary;
+	
+	private String[] departments = null;
 }
