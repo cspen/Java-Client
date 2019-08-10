7b5bbcdb19a8eacdb69e9bb3ff4f5608b2ce4fbc
unknown <cspen@modintro.com>
2019-08-09 21:52:31 -0500

    Add constants for for table columns

--- com/modintro/restfulclient/model/Constants.java
+++ com/modintro/restfulclient/model/Constants.java
@@ -9,5 +9,18 @@ public interface Constants {
 	
 	String APP_URL = "http://modintro.com/employees/";
 	String DEPT_URL = "http://modintro.com/departments/";
+	
+	Object[] colNames = {"EmployeeID", "Last_name", "First_name",
+			"Department", "Full_time", "Hire_date", "Salary" };
+	
+	int ID_COL = 0;
+	int FIRST_NAME_COL = 1;
+	int LAST_NAME_COL = 2;
+	int DEPARTMENT_COL = 3;
+	int FULL_TIME_COL = 4;
+	int HIRE_DATE_COL = 5;
+	int SALARY_COL = 6;
+	int ETAG_COL = 7;
+	int LAST_MOD_COL = 8;
 
 }
